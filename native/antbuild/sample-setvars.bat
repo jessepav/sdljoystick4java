6bf6ed7d29d92b4cd72a7d19067814d8c4ff49aa
Jesse Pavel <jpavel@alum.mit.edu>
2021-04-25 18:59:46 -0400

    Remove old batch file build

--- native/antbuild/sample-setvars.bat
+++
@@ -1,5 +0,0 @@
-:: Copy this file to 'setvars.bat' and set these variables appropriately
-
-set SDL_DIR=d:\Relocations\Code\Tools\SDL2-2.0.12
-set JDK_HOME_X86=c:\Program Files (x86)\Java\jdk1.7.0_261
-set JDK_HOME_X64=c:\Program Files\Java\jdk1.7.0_80
